6262264ba8844d1f4e62eec530c828f620a8a987
tiendat365 <23010200@st.phenikaa-uni.edu.vn>
2025-06-10 10:41:19 +0700

    initial commit

---
+++ webbanlinhkienmttL9/app/Models/Customer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $table = 'customers';
+    protected $primaryKey = 'id';
+    protected $fillable = ['userID','name','email','address','phone_number','note'];
+}
---
+++ webbanlinhkienmttL9/resources/views/layout/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Edit by Nguyễn Thành Trung</strong>
+    
+    <div class="float-right d-none d-sm-inline-block">
+     
+    </div>
+  </footer>
